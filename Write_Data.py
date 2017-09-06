9413356f5f307d7735881ba1af9ac9e6ca4d066b
jchxu <jchxu@qq.com>
2017-09-06 17:19:23 +0800

    修正已有历史数据追踪文件时，合并单元格被拆分的bug

--- Write_Data.py
+++ Write_Data.py
@@ -293,7 +293,9 @@ def write_tracking(stddate, trackfile, subsheet, rowindex, powder, block, totalr
     day = stddate[2:4]
     date = "%4s/%2s/%2s" % (year, month, day)
     # 标题部分
+    titleflag = 0
     if rowindex == 0:
+        titleflag = 1
         rowindex += 4
         subsheet.write_merge(0, rowindex, 0, 0, u"日期")
         subsheet.write_merge(0, rowindex, 1, 1, u"总库存")
@@ -361,19 +363,20 @@ def write_tracking(stddate, trackfile, subsheet, rowindex, powder, block, totalr
     subsheet.write(rowindex, 16 + 6 * (len(powder) + len(block)), nonmainrow[5])
 
     # 合并标题单元格
-    subsheet.write_merge(0, 4, 0, 0, u"日期")
-    subsheet.write_merge(0, 4, 1, 1, u"总库存")
-    subsheet.write_merge(1, 4, 2, 2, u"主流")
-    subsheet.write_merge(2, 4, 4, 4, u"粉矿")
-    for i in range(0, len(powder)):
-        subsheet.write_merge(3, 4, 6 + i * 6, 6 + i * 6, powder[i])
-    subsheet.write_merge(2, 4, 6 + 6 * len(powder), 6 + 6 * len(powder), u"块矿")
-    for i in range(0, len(block)):
-        subsheet.write_merge(3, 4, 8 + 6 * len(powder) + i * 6, 8 + 6 * len(powder) + i * 6, block[i])
-    subsheet.write_merge(2, 4, 8 + 6 * (len(powder) + len(block)), 8 + 6 * (len(powder) + len(block)), u"钢厂资源")
-    subsheet.write_merge(2, 4, 10 + 6 * (len(powder) + len(block)), 10 + 6 * (len(powder) + len(block)), u"贸易商资源")
-    subsheet.write_merge(1, 4, 12 + 6 * (len(powder) + len(block)), 12 + 6 * (len(powder) + len(block)), u"非主流")
-    subsheet.write_merge(2, 4, 13 + 6 * (len(powder) + len(block)), 13 + 6 * (len(powder) + len(block)), u"钢厂资源")
-    subsheet.write_merge(2, 4, 15 + 6 * (len(powder) + len(block)), 15 + 6 * (len(powder) + len(block)), u"贸易商资源")
+    if titleflag == 0:
+        subsheet.write_merge(0, 4, 0, 0, u"日期")
+        subsheet.write_merge(0, 4, 1, 1, u"总库存")
+        subsheet.write_merge(1, 4, 2, 2, u"主流")
+        subsheet.write_merge(2, 4, 4, 4, u"粉矿")
+        for i in range(0, len(powder)):
+            subsheet.write_merge(3, 4, 6 + i * 6, 6 + i * 6, powder[i])
+        subsheet.write_merge(2, 4, 6 + 6 * len(powder), 6 + 6 * len(powder), u"块矿")
+        for i in range(0, len(block)):
+            subsheet.write_merge(3, 4, 8 + 6 * len(powder) + i * 6, 8 + 6 * len(powder) + i * 6, block[i])
+        subsheet.write_merge(2, 4, 8 + 6 * (len(powder) + len(block)), 8 + 6 * (len(powder) + len(block)), u"钢厂资源")
+        subsheet.write_merge(2, 4, 10 + 6 * (len(powder) + len(block)), 10 + 6 * (len(powder) + len(block)), u"贸易商资源")
+        subsheet.write_merge(1, 4, 12 + 6 * (len(powder) + len(block)), 12 + 6 * (len(powder) + len(block)), u"非主流")
+        subsheet.write_merge(2, 4, 13 + 6 * (len(powder) + len(block)), 13 + 6 * (len(powder) + len(block)), u"钢厂资源")
+        subsheet.write_merge(2, 4, 15 + 6 * (len(powder) + len(block)), 15 + 6 * (len(powder) + len(block)), u"贸易商资源")
 
     return trackfile
