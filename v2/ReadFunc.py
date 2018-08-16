a0c13897a431f4350181a05dd88ba3e2fc9553c3
jchxu <jchxu@qq.com>
2018-08-16 15:35:11 +0800

    格式化数据v2 读取港口数据 读取分类名称 读取标准名称

--- v2/ReadFunc.py
+++ v2/ReadFunc.py
@@ -1,6 +1,29 @@
 # coding=utf-8
 import xlrd
 
+### 读取原始数据，返回列表 ###
+def ReadSource(SourceFileName):
+    Owner = []
+    Goods = []
+    Amount = []
+    Port = []
+    ArrivalDate = []
+    SourceFile = xlrd.open_workbook(SourceFileName, 'r')
+    Sheet = SourceFile.sheet_by_index(0)
+    for i in range(1, Sheet.nrows):
+        Line = Sheet.row_values(i)
+        Owner.append(Line[0])
+        Goods.append(Line[1])
+        Amount.append(Line[2])
+        Port.append(Line[3])
+        if len(Line) >= 5:  #有日期，记录日期；无日期，记录为“-”
+            ArrivalDate.append(Line[4])
+        else:
+            ArrivalDate.append('-')
+    print(u'已读取"\033[1;34;0m%s\033[0m"中的\033[1;34;0m%d\033[0m条数据.' % (SourceFileName, Sheet.nrows-1))
+    SourceFile.release_resources()
+    return (Owner, Goods, Amount, Port, ArrivalDate)
+
 ### 读取分类名录中的各个子表，返回为列表，主流粉/块等返回{分类名称：品种}字典
 def ReadList(ListFileName):
     GoodsClassName = {}
