5e12aee1c88b00b313ff981ffbc4ffff921b1806
jchxu <jchxu@qq.com>
2018-08-21 15:49:18 +0800

    测试书写xlsx

--- v2/FormatData_v2.py
+++ v2/FormatData_v2.py
@@ -5,7 +5,7 @@ from WriteFunc import *
 
 ### 需要用户定义的参数 ###
 SourceFile = "京唐港8.1库存.xlsx"   #港口数据文件
-#SourceFile = "京唐港01.07库存.xlsx"   #港口数据文件
+SourceFile = "京唐港01.07库存.xlsx"   #港口数据文件
 ListFile = "分类名录.xlsx"  #记录主流粉矿、主流块矿、非主流资源、品种、钢厂、贸易商名录的文件
 StdFile = "标准名称.xlsx"  #记录货主（钢厂、贸易商）、品种标准名称的数据文件
 StdPort = [u'曹妃甸',u'京唐',u'岚桥',u'岚山',u'连云港',u'青岛',u'日照']
--- v2/WriteFunc.py
+++ v2/WriteFunc.py
@@ -41,40 +41,53 @@ def TopShip(GoodShipDict):
 ### 打印输出各港口、分类、品种汇总数量、货权集中度 （屏幕输出、csv、txt版）###
 #AmountInfo = [TotalAmount, TotalSteel, ClassTotal, ClassSteel, GoodsTotal, GoodsSteel]
 #ShipInfo = [GoodShip, GoodSteelShip, GoodOtherShip]
+#def WriteSummary(AmountInfo,ShipInfo, PortList, GoodsClassName, GoodsClassList):
+#    for i in range(0,len(PortList)):
+#        print(PortList[i])
+#        port = PortList[i]
+#        totalamount = AmountInfo[0][port]
+#        totalsteel = AmountInfo[1][port]
+#        totaltrade = totalamount - totalsteel
+#        steeltotalratio, tradetotalratio = CalcRatio(totalamount, totalsteel, totaltrade)
+#        print("%s,%.0f,%.0f,%.1f%%,%.0f,%.1f%%" % ('合计', totalamount, totalsteel, steeltotalratio, totaltrade, tradetotalratio), sep=',')
+#        for j in range(0,len(GoodsClassName)):
+#            classname = GoodsClassName[j]
+#            calsstotal = AmountInfo[2][port][classname]
+#            classsteel = AmountInfo[3][port][classname]
+#            classtrade = calsstotal - classsteel
+#            steelratio, traderatio = CalcRatio(calsstotal, classsteel, classtrade)
+#            print("%s,%.0f,%.0f,%.1f%%,%.0f,%.1f%%" % (classname,calsstotal,classsteel, steelratio, classtrade, traderatio), sep=',')
+#            if (j == 0) or (j == 1): #pass
+#                for k in range(0,len(GoodsClassList[j])):
+#                    goodname = GoodsClassList[j][k]
+#                    if goodname in AmountInfo[4][port].keys():
+#                        goodtotal = AmountInfo[4][port][goodname]
+#                        goodsteel = AmountInfo[5][port][goodname]
+#                        goodtrade = goodtotal - goodsteel
+#                        goodship = ShipInfo[0][port][goodname]
+#                        goodsteelship = ShipInfo[1][port][goodname]
+#                        goodothership = ShipInfo[2][port][goodname]
+#                        GoodTop13, GoodTop46, GoodTopOther = TopShip(goodship)
+#                        GoodSteelTop13, GoodSteelTop46, GoodSteelTopOther = TopShip(goodsteelship)
+#                        GoodOtherTop13, GoodOtherTop46, GoodOtherTopOther = TopShip(goodothership)
+#                    else:
+#                        goodtotal = 0
+#                        goodsteel = 0
+#                        goodtrade = 0
+#                        GoodTop13, GoodTop46, GoodTopOther = ('','','')
+#                    steelratio, traderatio = CalcRatio(goodtotal, goodsteel, goodtrade)
+#                    print("%s,%.0f,%.0f,%.1f%%,%.0f,%.1f%%" % (goodname, goodtotal, goodsteel, steelratio, goodtrade, traderatio), sep=',', end = ',')
+#                    print("%s,%s,%s,%s,%s,%s,%s,%s,%s" % (GoodTop13, GoodTop46, GoodTopOther,GoodSteelTop13, GoodSteelTop46, GoodSteelTopOther,GoodOtherTop13, GoodOtherTop46, GoodOtherTopOther), sep=',')
+#
+###
 def WriteSummary(AmountInfo,ShipInfo, PortList, GoodsClassName, GoodsClassList):
-    for i in range(0,len(PortList)):
-        print(PortList[i])
+    Filename = 'test.xlsx'
+    WorkBook = xlsxwriter.Workbook(Filename)
+    Sheet1 = WorkBook.add_worksheet()
+
+    for i in range(0, len(PortList)):
+    # print(PortList[i])
         port = PortList[i]
-        totalamount = AmountInfo[0][port]
-        totalsteel = AmountInfo[1][port]
-        totaltrade = totalamount - totalsteel
-        steeltotalratio, tradetotalratio = CalcRatio(totalamount, totalsteel, totaltrade)
-        print("%s,%.0f,%.0f,%.1f%%,%.0f,%.1f%%" % ('合计', totalamount, totalsteel, steeltotalratio, totaltrade, tradetotalratio), sep=',')
-        for j in range(0,len(GoodsClassName)):
-            classname = GoodsClassName[j]
-            calsstotal = AmountInfo[2][port][classname]
-            classsteel = AmountInfo[3][port][classname]
-            classtrade = calsstotal - classsteel
-            steelratio, traderatio = CalcRatio(calsstotal, classsteel, classtrade)
-            print("%s,%.0f,%.0f,%.1f%%,%.0f,%.1f%%" % (classname,calsstotal,classsteel, steelratio, classtrade, traderatio), sep=',')
-            if (j == 0) or (j == 1): pass
-            for k in range(0,len(GoodsClassList[j])):
-                goodname = GoodsClassList[j][k]
-                if goodname in AmountInfo[4][port].keys():
-                    goodtotal = AmountInfo[4][port][goodname]
-                    goodsteel = AmountInfo[5][port][goodname]
-                    goodtrade = goodtotal - goodsteel
-                    goodship = ShipInfo[0][port][goodname]
-                    goodsteelship = ShipInfo[1][port][goodname]
-                    goodothership = ShipInfo[2][port][goodname]
-                    GoodTop13, GoodTop46, GoodTopOther = TopShip(goodship)
-                    GoodSteelTop13, GoodSteelTop46, GoodSteelTopOther = TopShip(goodsteelship)
-                    GoodOtherTop13, GoodOtherTop46, GoodOtherTopOther = TopShip(goodothership)
-                else:
-                    goodtotal = 0
-                    goodsteel = 0
-                    goodtrade = 0
-                    GoodTop13, GoodTop46, GoodTopOther = ('','','')
-                steelratio, traderatio = CalcRatio(goodtotal, goodsteel, goodtrade)
-                print("%s,%.0f,%.0f,%.1f%%,%.0f,%.1f%%" % (goodname, goodtotal, goodsteel, steelratio, goodtrade, traderatio), sep=',', end = ',')
-                print("%s,%s,%s,%s,%s,%s,%s,%s,%s" % (GoodTop13, GoodTop46, GoodTopOther,GoodSteelTop13, GoodSteelTop46, GoodSteelTopOther,GoodOtherTop13, GoodOtherTop46, GoodOtherTopOther), sep=',')
+        Sheet1.write(0,i*9,port)
+
+    WorkBook.close()
